0b5477b845b2e1283c844043771dcfea9542286e
unknown <kasemadel8@gmail.com>
2023-01-13 18:12:19 +0200

    fix test frontend and backend

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -8,8 +8,12 @@ describe('<LoadingMessage>', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
         const wrapper = shallow(<LoadingMessage message={message} />);
-        expect(wrapper.contains(<span>{message}?</span>)).toBeTruthy(); //remove the question mark to make the test pass
+        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); //remove the question mark to make the test pass
       });
     });
   });
 });
+function expect(arg0: any) {
+  throw new Error('Function not implemented.');
+}
+
